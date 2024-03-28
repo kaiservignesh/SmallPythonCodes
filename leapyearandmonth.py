85aca43c8f212d8aa7421594741e722aee0e01e7
kaiservignesh <kaiservignesh@gmail.com>
2024-03-28 06:39:50 +0530

    Create leapyearandmonth.py

---
+++ leapyearandmonth.py
@@ -0,0 +1,32 @@
+def is_year_leap(year):
+    if year % 4 != 0:
+        return False
+    elif year % 100 != 0:
+        return True
+    elif year % 400 != 0:
+        return False
+    else:
+        return True
+
+def days_in_month(year, month):
+    if (is_year_leap(year)==True) and (month==2):
+        return 29
+    elif (is_year_leap(year)==False) and (month==2):
+        return 28
+    elif (month in [1,3,5,6,8,10,12]):
+        return 31
+    else:
+        return 30
+
+test_years = [1900, 2000, 2016, 1987]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
+for i in range(len(test_years)):
+    yr = test_years[i]
+    mo = test_months[i]
+    print(yr, mo, "->", end="")
+    result = days_in_month(yr, mo)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")
